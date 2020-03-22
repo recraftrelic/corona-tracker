a43e089b124bfd474fc8be06d7b72bb3e3fd60e5
Manoj Singh Negi <justanothermanoj@gmail.com>
2020-03-22 15:09:42 +0530

    added global stats

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import './normalize.css';
+import './skeleton.css';
 import './App.css';
 
 function App() {
+
+  const [globalData, setGlobalData] = useState({})
+
+  const getAndSetGlobalData = () => {
+    axios("https://thevirustracker.com/free-api?global=stats")
+      .then(response => {
+        setGlobalData(response.data.results[0])
+      })
+  }
+
+  useEffect(() => {
+    getAndSetGlobalData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1>Global records</h1>
+      <div class="row">
+        <div class="six columns"><h5>Total Cases</h5></div>
+        <div class="six columns"><h5>{globalData.total_cases}</h5></div>
+      </div>
+      <div class="row">
+        <div class="six columns"><h5>Active Cases</h5></div>
+        <div class="six columns"><h5>{globalData.total_active_cases}</h5></div>
+      </div>
+      <div class="row">
+        <div class="six columns"><h5>Total Deaths</h5></div>
+        <div class="six columns"><h5>{globalData.total_deaths}</h5></div>
+      </div>
+      <div class="row">
+        <div class="six columns"><h5>New Cases Today</h5></div>
+        <div class="six columns"><h5>{globalData.total_new_cases_today}</h5></div>
+      </div>
+      <div class="row">
+        <div class="six columns"><h5>New Deaths Today</h5></div>
+        <div class="six columns"><h5>{globalData.total_new_deaths_today}</h5></div>
+      </div>
+      <div class="row">
+        <div class="six columns"><h5>Total Recovered Cases</h5></div>
+        <div class="six columns"><h5>{globalData.total_recovered}</h5></div>
+      </div>
     </div>
   );
 }
