4c364a7c43eae095fef7b22b36bac1630cc40f97
Manoj Singh Negi <justanothermanoj@gmail.com>
2020-03-22 15:18:37 +0530

    added colors

--- src/App.js
+++ src/App.js
@@ -27,24 +27,24 @@ function App() {
         <div class="six columns"><h5>{globalData.total_cases}</h5></div>
       </div>
       <div class="row">
-        <div class="six columns"><h5>Active Cases</h5></div>
-        <div class="six columns"><h5>{globalData.total_active_cases}</h5></div>
-      </div>
-      <div class="row">
-        <div class="six columns"><h5>Total Deaths</h5></div>
-        <div class="six columns"><h5>{globalData.total_deaths}</h5></div>
+        <div class="six columns"><h5>Total Recovered Cases</h5></div>
+        <div class="six columns"><h5 className="green">{globalData.total_recovered}</h5></div>
       </div>
       <div class="row">
         <div class="six columns"><h5>New Cases Today</h5></div>
-        <div class="six columns"><h5>{globalData.total_new_cases_today}</h5></div>
+        <div class="six columns"><h5 className="red">{globalData.total_new_cases_today}</h5></div>
       </div>
       <div class="row">
         <div class="six columns"><h5>New Deaths Today</h5></div>
-        <div class="six columns"><h5>{globalData.total_new_deaths_today}</h5></div>
+        <div class="six columns"><h5 className="red">{globalData.total_new_deaths_today}</h5></div>
       </div>
       <div class="row">
-        <div class="six columns"><h5>Total Recovered Cases</h5></div>
-        <div class="six columns"><h5>{globalData.total_recovered}</h5></div>
+        <div class="six columns"><h5>Active Cases</h5></div>
+        <div class="six columns"><h5 className="red">{globalData.total_active_cases}</h5></div>
+      </div>
+      <div class="row">
+        <div class="six columns"><h5>Total Deaths</h5></div>
+        <div class="six columns"><h5 className="red">{globalData.total_deaths}</h5></div>
       </div>
     </div>
   );
