91a4e195b5b95c7339e70724d882368cbc7879ac
Manoj Singh Negi <justanothermanoj@gmail.com>
2020-03-22 18:14:42 +0530

    added news

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
+import ReactLoading from 'react-loading';
+import RecordDetails from './components/RecordDetails';
+import { countries } from './constants';
+
 import './normalize.css';
 import './skeleton.css';
 import './App.css';
-import RecordDetails from './components/RecordDetails';
-import { countries } from './constants';
 
 function App() {
 
   const [record, setRecord] = useState({})
   const [region, setRegion] = useState("world")
+  const [loading, setLoading] = useState(true)
+  const [newsItems, setNewsItems] = useState([])
 
   const getAndSetGlobalData = () => {
     axios("https://thevirustracker.com/free-api?global=stats")
       .then(response => {
+        setLoading(false)
         setRecord({
           ...response.data.results[0],
           info: {
@@ -21,6 +26,7 @@ function App() {
           }
         })
       })
+      .catch(() => setLoading(false))
   }
 
   useEffect(() => {
@@ -28,18 +34,40 @@ function App() {
   }, [])
 
   useEffect(() => {
+    setLoading(true)
     if (region == "world") {
       getAndSetGlobalData()
     } else {
       axios(`https://thevirustracker.com/free-api?countryTotal=${region}`)
-        .then(response => setRecord(response.data.countrydata[0]))
+        .then(response => {
+          setLoading(false)
+          setRecord(response.data.countrydata[0])
+          // setNewsItems(response.data)
+
+          const news = Object.keys(response.data.countrynewsitems[0])
+          .reduce((result, item) => {
+            return result.concat(response.data.countrynewsitems[0][item])
+          }, [])
+
+          setNewsItems(news)
+        })
+        .catch(() => setLoading(false))
     }
   }, [region])
+
+  if (loading) {
+    return (
+      <div className="loading-container u-full-width">
+        <ReactLoading type="spin" color="#333" />
+      </div>
+    )
+  }
 
   return (
     <div className="container">
       <div className="row">
         <div className="twelve columns">
+          <h3>Select country</h3>
           <select
             value={region}
             onChange={e => setRegion(e.target.value)}
@@ -56,6 +84,21 @@ function App() {
       <RecordDetails
         record={record}
       />
+      <h3>News</h3>
+      {
+        newsItems.map(
+          (news, index) => (
+            <div key={index} className="row">
+              <div className="two columns">
+                <img className="u-max-full-width" src={news.image} />
+              </div>
+              <div className="ten columns">
+                <h5 dangerouslySetInnerHTML={{__html: news.title}}></h5>
+              </div>
+            </div>
+          )
+        )
+      }
     </div>
   );
 }
